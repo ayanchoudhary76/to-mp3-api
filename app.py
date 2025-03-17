d668fb2651536cb347be13976d0fcc48947bd6cb
Ayan Choudhary <145195782+ayanchoudhary76@users.noreply.github.com>
2025-03-17 10:04:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def index():
     """
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000)) 
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))  
+    os.system(f"gunicorn --bind 0.0.0.0:{port} --timeout 0 app:app")
